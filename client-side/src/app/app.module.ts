f20af16d1ab64229558b29cd4a7841f2e6d3c1ac
Chasky Hoffmann <Chasky.h@pepperi.com>
2022-03-23 20:04:25 +0200

    added generic list added spec endpoint

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -16,13 +16,17 @@ import { PepSizeDetectorModule} from '@pepperi-addons/ngx-lib/size-detector';
 
 import { PepTopBarModule } from '@pepperi-addons/ngx-lib/top-bar';
 import { PepPageLayoutModule } from '@pepperi-addons/ngx-lib/page-layout';
+import { PepButtonModule } from '@pepperi-addons/ngx-lib/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ApiCollectionsComponent } from './api-collections/api-collections.component';
+import { PepGenericListModule } from '@pepperi-addons/ngx-composite-lib/generic-list';
 
 @NgModule({
     declarations: [
         AppComponent,
         AddonComponent,
         SwaggerUiComponent,
+        ApiCollectionsComponent,
     ],
     imports: [
         BrowserModule,
@@ -33,7 +37,9 @@ import { MatTabsModule } from '@angular/material/tabs';
         PepSizeDetectorModule,
         PepTopBarModule,
         PepPageLayoutModule,
+        PepButtonModule,
         MatTabsModule,
+        PepGenericListModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
